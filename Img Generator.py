63897e9d94c8353a707a166200ccf3a801b1db69
Sciguy324 <webmeyer@yahoo.com>
2019-12-14 15:37:24 -0500

    Started to add tilemap editing window.

---
+++ Img Generator.py
@@ -0,0 +1,35 @@
+import base64
+
+
+def main():
+    with open("test.gif", "wb") as f:
+        encoded_img = base64.decodebytes(b'''
+                R0lGODlhCAAIAMIEAAAAAP/SAP/bNNnZ2cbGxsbGxsbGxsbGxiH5BAEKAAQALAAA
+                AAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU5kEJADs=
+                ''')
+        f.write(encoded_img)
+
+    with open("test2.gif", "wb") as f:
+        encoded_img2 = base64.decodebytes(b'''
+                R0lGODlhCAAIAMIEAAAAAOUqKv9mZtnZ2Ts7Ozs7Ozs7Ozs7OyH+EUNyZWF0ZWQg
+                d2l0aCBHSU1QACH5BAEKAAQALAAAAAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU
+                5kEJADs=
+                ''')
+        f.write(encoded_img2)
+
+    with open("test3.gif", "wb") as f:
+        encoded_img3 = base64.decodebytes(b'''
+                R0lGODlhCAAIAMIBAAAAADs7O4+Pj9nZ2Ts7Ozs7Ozs7Ozs7OyH+EUNyZWF0ZWQg
+                d2l0aCBHSU1QACH5BAEKAAQALAAAAAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU
+                5kEJADs=
+                ''')
+        f.write(encoded_img3)
+
+    for i in ["close_active", "close_pressed", "close"]:
+        print(i + ": ")
+        with open(i + ".gif", "rb") as f:
+            print(base64.encodebytes(f.read()))
+
+
+if __name__ == "__main__":
+    main()
--- World Builder 2.py
+++ World Builder 2.py
@@ -2,12 +2,142 @@ import tkinter as tk
 from tkinter import ttk
 
 
+class CustomNotebook(ttk.Notebook):
+    """A ttk Notebook with close buttons on each tab.
+    https://stackoverflow.com/questions/39458337/is-there-a-way-to-add-close-buttons-to-tabs-in-tkinter-ttk-notebook"""
+
+    __initialized = False
+
+    def __init__(self, *args, **kwargs):
+        if not self.__initialized:
+            self.__initialize_custom_style()
+            self.__initialized = True
+
+        kwargs["style"] = "CustomNotebook"
+        ttk.Notebook.__init__(self, *args, **kwargs)
+
+        self._active = None
+
+        self.bind("<ButtonPress-1>", self.on_close_press, True)
+        self.bind("<ButtonRelease-1>", self.on_close_release)
+
+    def on_close_press(self, event):
+        """Called when the button is pressed over the close button"""
+
+        element = self.identify(event.x, event.y)
+
+        if "close" in element:
+            index = self.index("@%d,%d" % (event.x, event.y))
+            self.state(['pressed'])
+            self._active = index
+
+    def on_close_release(self, event):
+        """Called when the button is released over the close button"""
+        if not self.instate(['pressed']):
+            return
+
+        element = self.identify(event.x, event.y)
+        index = self.index("@%d,%d" % (event.x, event.y))
+
+        if "close" in element and self._active == index:
+            self.forget(index)
+            self.event_generate("<<NotebookTabClosed>>")
+
+        self.state(["!pressed"])
+        self._active = None
+
+    def __initialize_custom_style(self):
+        style = ttk.Style()
+        self.images = (
+            tk.PhotoImage("img_close", data='''
+                R0lGODlhCAAIAKEDAA4ODjs7O4+Pj8oAACH5BAEKAAMALAAAAAAIAAgAAAISDIYD
+                Zrj3XgJCnFmv\nMlJu7wQFADs=
+            '''),
+            tk.PhotoImage("img_closeactive", data='''R0lGODlhCAAIAKEBAEUAAMoAAMo
+            AAMoAACH5BAEKAAIALAAAAAAIAAgAAAIRDIQCZrj3XlpthqqM\nlJm7UAAAOw==
+            '''),
+            tk.PhotoImage("img_closepressed", data='''R0lGODlhCAAIAMIEAG4PD3wtLe
+            UqKv9mZsoAAMoAAMoAAMoAACH5BAEKAAQALAAAAAAIAAgAAAMX\nKEBMoIq99UKILIwR
+            GtFcU10UNmGSkAAAOw==
+            ''')
+        )
+
+        style.element_create("close", "image", "img_close",
+                             ("active", "pressed", "!disabled", "img_closepressed"),
+                             ("active", "!disabled", "img_closeactive"), border=8, sticky='')
+        style.layout("CustomNotebook", [("CustomNotebook.client", {"sticky": "nswe"})])
+        style.layout("CustomNotebook.Tab", [
+            ("CustomNotebook.tab", {
+                "sticky": "nswe",
+                "children": [
+                    ("CustomNotebook.padding", {
+                        "side": "top",
+                        "sticky": "nswe",
+                        "children": [
+                            ("CustomNotebook.focus", {
+                                "side": "top",
+                                "sticky": "nswe",
+                                "children": [
+                                    ("CustomNotebook.label", {"side": "left", "sticky": ''}),
+                                    ("CustomNotebook.close", {"side": "left", "sticky": ''}),
+                                ]
+                            })
+                        ]
+                    })
+                ]
+            })
+        ])
+
+
 class TilemapEditorWindow:
 
     def __init__(self, parent):
-        """Editor window for a level"""
-        self.panel = ttk.Frame(parent)
-        parent.add(self.panel, text="Map Editor")
+        """Overarching editor window for the levels"""
+        # Add editor to parent window
+        self.frame = ttk.Frame(parent)
+        self.frame.pack(fill=tk.X, expand=1)
+        parent.add(self.frame, text="Map Editor")
+
+        # Layout setup
+        # Set up level viewing section
+        self.tilemap_panel = CustomNotebook(self.frame)
+        self.tilemap_panel.grid(row=1, column=0, sticky=tk.NW)
+
+        # Add button panel
+        self.buttons = ttk.Frame(self.frame)
+        self.buttons.grid(row=0, column=0, sticky=tk.NW)
+        # self.close_view_button = ttk.Button(self.buttons, text="X", width=2, command=self.close_view)
+        # self.close_view_button.grid(row=0, column=0, sticky=tk.NW)
+        self.new_view_button = ttk.Button(self.buttons, text="+", width=2, command=self.new_view)
+        self.new_view_button.grid(row=0, column=1, sticky=tk.NW)
+
+        # Create initial tilemap view
+        self.new_view()
+
+    def new_view(self):
+        """Create a new, blank view"""
+        TilemapView(self.tilemap_panel)
+        self.tilemap_panel.select(self.tilemap_panel.index("end") - 1)
+
+    def close_view(self):
+        """Close the currently open view"""
+        self.tilemap_panel.forget("current")
+
+
+class TilemapView:
+
+    def __init__(self, parent):
+        """Sub-window for viewing individual levels"""
+        # Check if the parent is a notebook, which it SHOULD BE
+        if type(parent) != CustomNotebook:
+            raise TypeError("Cannot assign a {} to a {}".format(TilemapView, type(parent)))
+
+        # Create element layout
+        self.view = ttk.Frame(parent, borderwidth=1, relief=tk.SUNKEN)
+        self.canvas = tk.Canvas(self.view, width=1024, height=576, bg="WHITE", bd=0)
+        self.canvas.pack()
+
+        parent.add(self.view, text="*Untitled")
 
 
 class SelectionPane:
@@ -42,13 +172,16 @@ class App:
 
     def __init__(self, parent):
         """World Builder 2"""
-        # Begin editor setup
+        # Begin editor layout setup
         self.source = ttk.Notebook(parent)
 
-        tab1 = ttk.Frame(self.source)
+        # Create tilemap editing window
+        self.tilemap_editor = TilemapEditorWindow(self.source)
+
+        # tab1 = ttk.Frame(self.source)
         tab2 = ttk.Frame(self.source)
 
-        self.source.add(tab1, text="Tab 1")
+        # self.source.add(tab1, text="Tab 1")
         self.source.add(tab2, text="Tab 2")
 
         self.source.pack(expand=1, fill="both")
